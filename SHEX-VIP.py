87a2493ffbdb7fb6e066c52a6fb158e1f0e9aeab
SHEX_CRACKER <77216010+SHEXA-CRACKER@users.noreply.github.com>
2021-01-14 18:11:11 +0300

    Update SHEX-VIP.py

--- SHEX-VIP.py
+++ SHEX-VIP.py
@@ -416,7 +416,6 @@ def pilih_super():
 	print "\033[1;32;40m[+] Total CRACKED/\x1b[1;93mCHECKPOINT \033[1;91m: \033[1;91m"+str(len(oks))+"\033[1;31;40m/\033[1;36;40m"+str(len(cekpoint))
 	print '\033[1;34;40m[+] CP File Has Been Saved : save/cp.txt'
 	print """
-        except requests.exceptions.ConnectionError:
         print '\x1b[1;91m[!] Connection Error'
         time.sleep(1)
         except IOError:
