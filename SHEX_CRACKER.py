6c20aa39daa6d0f296fa43ce03b1c030ad348843
SHEX_CRACKER <77216010+SHEXA-CRACKER@users.noreply.github.com>
2021-01-15 23:53:30 +0300

    SHEX_CRACKER.py

--- SHEX_CRACKER.py
+++ SHEX_CRACKER.py
@@ -5,29 +5,21 @@ import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,re
 from multiprocessing.pool import ThreadPool
 from requests.exceptions import ConnectionError
 from mechanize import Browser
-
-
 reload(sys)
 sys.setdefaultencoding('utf8')
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
 br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
-
-
 def keluar():
 	print "\033[1;96m[!] \x1b[1;91mExit"
 	os.sys.exit()
-
-
 def acak(b):
     w = 'ahtdzjc'
     d = ''
     for i in x:
         d += '!'+w[random.randint(0,len(w)-1)]+i
     return cetak(d)
-
-
 def cetak(b):
     w = 'ahtdzjc'
     for i in w:
@@ -36,15 +28,11 @@ def cetak(b):
     x += '\033[0m'
     x = x.replace('!0','\033[0m')
     sys.stdout.write(x+'\n')
-
-
 def jalan(z):
 	for e in z + '\n':
 		sys.stdout.write(e)
 		sys.stdout.flush()
 		time.sleep(00000.1)
-
-
 #### LOGO ####
 logo = """
 \033[91m
@@ -61,8 +49,6 @@ def tik():
 	titik = ['.   ','..  ','... ']
 	for o in titik:
 		print("\r\033[1;96m[●] \x1b[1;93mSedang masuk \x1b[1;97m"+o),;sys.stdout.flush();time.sleep(1)
-
-
 back = 0
 berhasil = []
 cekpoint = []
@@ -78,7 +64,6 @@ print  """\033[91mNote[Ema Bar Prs Nen La Hech Keshayak
 \033[33mTool Vip Bo Ftoshtn Nrx 10$
 \033[91mBo Kren @SHEX_CRACKER
 print " \x1b[1;93m⚔══════════════════════════☠═══════════════════════════⚔"
-
 CorrectUsername = "BLND"
 CorrectPassword = "MASUD"
 
